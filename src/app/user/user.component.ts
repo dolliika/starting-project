dc417b3224413235e799ce1c9db948c4ec21b459
Lan Nguyen <str4wbxrryymilk@gmail.com>
2025-12-29 10:51:31 +0700

    i forgot but a lot of stuff

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,5 +7,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({ required: true }) id!: string;
+  @Input({ required: true })
+  avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
